9000f9fec1bd85dcc815093fb5052c96cc640aa3
Manuel Casanova <manucasanova@hotmail.com>
2022-02-21 13:09:23 -0800

    MOVED THE FUNCTION CONNECT TO CLIENT.JS AND I REQUIRE IT IN THIS FILE

--- play.js
+++ play.js
@@ -1,20 +1,9 @@
-const net = require("net");
+
+//const net = require("net");
+
+const {connect} = require("./client");
 
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "10.0.0.123",
-    port: "50541"
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("connect", () => {
-console.log("Just a second, You are getting connected...")
-  });
-
-  return conn;
-};
 
 connect();
