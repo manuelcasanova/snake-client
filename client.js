12ccde47409c613518bf517d2eef919bfa1c175d
Manuel Casanova <manucasanova@hotmail.com>
2022-02-21 14:11:58 -0800

    SEND INITIALS TO SNAKE AND LOG A MESSAGE WHEN CONNECTED

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ const net = require("net");
 //const { builtinModules } = require("module");
 
 
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: "10.0.0.123",
     port: "50541"
@@ -11,16 +11,18 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
-//   conn.on("connect", () => {
-// console.log("Just a second, You are getting connected...")
-//   });
+  //   conn.on("connect", () => {
+  // console.log("Just a second, You are getting connected...")
+  //   });
 
-conn.on("connect", () => {
-  console.log("Successfully connected to game server")
-    });
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: MCF"); //Sends initials to the snake
+  });
 
   return conn;
 };
+console.log("Just a moment, you are getting connected...");
 
 module.exports = {
   connect
