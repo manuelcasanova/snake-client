b6a502d1142fb5e47fce424cbcb0049c9a8f9cbf
Manuel Casanova <manucasanova@hotmail.com>
2022-02-21 15:28:24 -0800

    EVENT LISTENER ADDED. FUNCTION HANDLEUSERINPUT WORKS WHEN RECEIVES INPUT FROM THE KEYBOARD

--- play.js
+++ play.js
@@ -1,9 +1,13 @@
-
-//const net = require("net");
-
 const {connect} = require("./client");
-
-// establishes a connection with the game server
-
-
 connect();
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput); //This is an event listener. The function will work when receives input from the keyboard.
+  return stdin;
+};
+
+
