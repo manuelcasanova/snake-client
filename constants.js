b1d1b49c97628ae75a6a3c71c34ba6059b345c04
Manuel Casanova <manucasanova@hotmail.com>
2022-02-22 05:57:21 -0800

    CONSTANTS TO SEND MESSAGES ADDED

--- constants.js
+++ constants.js
@@ -1,10 +1,15 @@
+const { OutgoingMessage } = require("http");
+
 const IP = "10.0.0.123";
 const PORT = "50541";
 const UP = "Move: up";
 const LEFT = "Move: left";
 const DOWN = "Move: down";
 const RIGHT = "Move: right";
-
+const message = "Say: ";
+const hello = "Hello!"
+const hola = "Hola!"
+const bye = "Bye!"
 
 
 
@@ -15,5 +20,9 @@ module.exports = {
   UP,
   LEFT,
   DOWN,
-  RIGHT
+  RIGHT,
+  message,
+  hello,
+  hola,
+  bye
 };
