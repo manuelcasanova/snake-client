ec2fc522e578b6d3a38e98e57a3e686cdf966c40
Manuel Casanova <manucasanova@hotmail.com>
2022-02-22 05:07:11 -0800

    IF STATEMENTS IF KEY PRESSED STRING PRINTED

--- input.js
+++ input.js
@@ -1,10 +1,10 @@
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  } 
-};
+const { stdout } = require("process");
 
-const setupInput = function () {
+let connection; //Stores the active TCP connection object. Otherwise the input module cannot send messages to the server.
+
+
+const setupInput = function (conn) {
+connection = conn; //changes setupInput() to accept an object that lets you interact with the server. 
 const stdin = process.stdin;
 stdin.setRawMode(true);
 stdin.setEncoding("utf8");
@@ -12,5 +12,31 @@ stdin.resume();
 stdin.on("data", handleUserInput); //This is an event listener. The function will work when receives input from the keyboard.
 return stdin;
 };
+
+//Bind the movement commands to the w a s d keys
+
+const w = "Move: up";
+const a = "Move: left";
+const s = "Move: down";
+const d = "Move: right";
+
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  } else if (key === "w") {
+    stdout.write(w) 
+  } else if (key === "a") {
+    stdout.write(a) 
+  } else if (key === "s") {
+    stdout.write(s) 
+  } else if (key === "d") {
+    stdout.write(d) 
+  }
+
+
+
+
+};
+
 
 module.exports = {setupInput};
