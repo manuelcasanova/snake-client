1cb0d4e3211c07482ad7a601a8f69b994b61aa20
Manuel Casanova <manucasanova@hotmail.com>
2022-02-21 17:08:57 -0800

    FUNCTIONS SETUP INPUT AND HANDLE USER INPUT MOVED TO NEW FILE INPUT.JS

--- play.js
+++ play.js
@@ -1,21 +1,9 @@
 const {connect} = require("./client");
+const {setupInput} = require("./input");
+
+
+console.log("Connecting...");
 connect();
-
-const handleUserInput = function (key) {
-    if (key === '\u0003') {
-      process.exit();
-    } 
-};
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput); //This is an event listener. The function will work when receives input from the keyboard.
-  return stdin;
-};
-
 setupInput(); //call the function
 
 
