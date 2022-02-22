79ee27fb3d4018d7e914a521c6591a86b9d162e0
Manuel Casanova <manucasanova@hotmail.com>
2022-02-22 05:09:47 -0800

    PASS THE OBJECT RETURNED FROM CONNECT TO THE SETUPINPUT FUNCTION

--- play.js
+++ play.js
@@ -1,9 +1,8 @@
 const {connect} = require("./client");
 const {setupInput} = require("./input");
 
+//connect();
+let c = connect();
+setupInput(c); //pass the object returned from connect into the setupInput function.
 
-console.log("Connecting...");
-connect();
-setupInput(); //call the function
-
-
+module.exports = {connect};
