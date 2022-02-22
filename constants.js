ad46fab10b801a005651883cf19b6898aab4407e
Manuel Casanova <manucasanova@hotmail.com>
2022-02-22 05:48:58 -0800

    ADDED W A S D CONSTANTS TO MOVE SNAKE

--- constants.js
+++ constants.js
@@ -1,6 +1,9 @@
 const IP = "10.0.0.123";
 const PORT = "50541";
-
+const UP = "Move: up";
+const LEFT = "Move: left";
+const DOWN = "Move: down";
+const RIGHT = "Move: right";
 
 
 
@@ -9,4 +12,8 @@ const PORT = "50541";
 module.exports = {
   IP,
   PORT,
+  UP,
+  LEFT,
+  DOWN,
+  RIGHT
 };
